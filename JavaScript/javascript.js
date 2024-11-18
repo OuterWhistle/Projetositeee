3d0f20718b58b2641b6600d7a7e488e6e65d39ff
OuterWhistle <107567881+OuterWhistle@users.noreply.github.com>
2024-11-18 18:24:11 -0300

    Add files via upload

---
+++ JavaScript/javascript.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const date = new Date();
+
+    const formattedDate = date.toLocaleDateString("pt-BR", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+    });
+
+    const dayOfWeek = date.getDay(); 
+
+    const dateDisplayElement = document.getElementById("date-display");
+
+    if (dateDisplayElement) {
+        if (dayOfWeek >= 0 & dayOfWeek <= 5) {
+            dateDisplayElement.textContent = `Hoje é ${formattedDate} - Estamos Abertos!`;
+        } else {
+            dateDisplayElement.textContent = `Hoje é ${formattedDate} - Estamos Fechados!`;
+        }
+    }
+});
